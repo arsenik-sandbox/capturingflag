81c33e0952e9c2f62dfd0a61419f71fb12a43f11
Beren Ç <92385837+arsenik-sandbox@users.noreply.github.com>
2023-07-21 02:01:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,207 @@
-/* TOGGLE ICON NAVBAR */
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('#navbar');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Capturing Flag | Beren Çoban</title>
 
-menuIcon.onclick = () => {
-menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
-};
+    <!-- custom css -->
+    <link rel="stylesheet" href="style.css">
 
-/* SCROLL SECTIONS ACTIVE LINK */
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+    <!-- typed js -->
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11/lib/typed.min.js"></script>
+    
+    <!-- box icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-window.onscroll = () => {
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+    <!-- TEST -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">   
+</head>
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
+<body>
 
-    let header = document.querySelector('header');
+    <!-- header design -->
+    <header class="header">
+        <a href="#" class="logo">Capturing Flag</a>
 
-    header.classList.toggle('sticky',window.scrollY > 100);
+        <i class='bx bx-menu' id="menu-icon"></i>
 
-    /* REMOVE TOGGLE ICON AND NAVBAR WHEN CLICK NAVBAR LINK (SCROLL) */
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
+        <nav class="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#about">About</a>
+            <a href="#tutorial">Tutorial</a>
+            <a href="#gallery">Gallery</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+    <!-- home section design -->
+    <section class="home" id="home">
+        <div class="home-content">
+            <h3>Hello, It's Me</h3>
+            <h1>Beren Çoban</h1>
+            <h3>And I'm a <span class="multiple-text">Penetration Tester</span></h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquam, modi aspernatur placeat
+                architecto amet laundantium id.</p>
+            <div class="social-media">
+                <a href="https://github.com/arsenik-sandbox/" target="_blank"><i class='bx bxl-github' ></i></a>
+                <a href="https://tr.linkedin.com/in/beren-çoban" target="_blank"><i class='bx bxl-linkedin'></i></a>
+                <a href="#contact"><i class='bx bx-envelope' ></i></a>
+            </div>
+            <a href="#" class="btn">Download CV</a>
+        </div>
 
-/* SCROLL REVEAL */
-ScrollReveal({
-    // reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
+        <div class="home-img"></div>
+            <img src="" alt="">
+    </section>
+    
+    <!-- about section design -->
 
-ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .timeline-container, .gallery-box, .contact form', {origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="" alt="">
+        </div>
 
-/* TYPED JS */
-const typed = new typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
+        <div class="about-content">
+            <h2 class="heading">About <span>Me</span></h2>
+            <h3>Penetration Tester</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem voluptatem nemo placeat voluptas
+                expedita tempore molestias autem dicta debitis pariatur! Eum, delectus ipsum! Porro, ab ipsam unde sapiente
+                architecto possimus quibusdam ratione repudiandae molestiae dignissimos ullam. Temporibus quo rem illum, aut nulla nisi 
+                cumque illo necessitatibus?</p>
+            <a href="#about" class="btn">Read More</a>
+        </div>
+    </section>
+
+    <!-- timeline section design -->
+    <section class="timeline" id="timeline">
+        <h2 class="heading">My <span>Experiences</span></h2>
+        
+        <div class="timeline">
+            <div class="timeline-box">
+                <i class='bx bx-code-alt'></i>
+                <h3>Web Development</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum alias corporis necessitatibus soluta
+                    provident ipsam beatae cumque
+                    facere culpa velit?</p>
+                <a href="#" class="btn">Read More</a>
+            </div>      
+        
+            <div class="timeline-box">
+                <i class='bx bxs-paint' ></i>
+                <h3>Graphic Design</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum alias corporis necessitatibus soluta
+                    provident ipsam beatae cumque
+                    facere culpa velit?</p>
+                <a href="#" class="btn">Read More</a>
+            </div>
+        
+        
+            <div class="timeline-box">
+                <i class='bx bx-bar-chart-alt' ></i>
+                <h3>Digital Marketing</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum alias corporis necessitatibus soluta
+                    provident ipsam beatae cumque
+                    facere culpa velit?</p>
+                <a href="#" class="btn">Read More</a>
+            </div>   
+        </div>
+    </section>
+
+    <!-- gallery section design -->
+
+    <section class="gallery" id="gallery">
+        <h2 class="heading">Latest <span>Project</span></h2>
+        
+        <div class="gallery-container">
+            <div class="gallery-box">
+                <img src="thm.png" alt="">
+                <div class="gallery-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, autem!</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+            <div class="gallery-box">
+                <img src="thm.png" alt="">
+                <div class="gallery-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, autem!</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+            <div class="gallery-box">
+                <img src="thm.png" alt="">
+                <div class="gallery-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, autem!</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+            <div class="gallery-box">
+                <img src="thm.png" alt="">
+                <div class="gallery-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, autem!</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+            <div class="gallery-box">
+                <img src="thm.png" alt="">
+                <div class="gallery-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, autem!</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+            <div class="gallery-box">
+                <img src="thm.png" alt="">
+                <div class="gallery-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, autem!</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section design -->
+    <section class="contact" id="contact">
+        <h2 class="heading">Contact <span> Me!</span></h2>
+        <form action="#">
+            <div class="input-box">
+                <input type="text" placeholder="Full Name">
+                <input type="email" placeholder="Email Address">
+            </div>
+            <div class="input-box">
+                <input type="number" placeholder="Mobile Number">
+                <input type="text" placeholder="Email Subject">
+            </div>
+            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+            <input type="submit" value="Send Message" class="btn"
+        </form>
+    </section>
+
+    <!-- footer design -->
+
+    <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2023 by Capturing Flag | All Rights Reserved.</p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class='bx bx-up-arrow-alt' ></i></a>
+        </div>
+    </footer>
+
+    <!-- scroll reveal -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <!-- typed js -->
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+    <!-- custom js -->
+    <script src="script.js"></script>
+</body>
+
+</html>
